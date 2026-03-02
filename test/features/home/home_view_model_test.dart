382b7022809b7a8b47cb9df046d735c5b4bc6914
Amlsakr <amlsakr9@gmail.com>
2026-03-02 13:20:56 +0200

    Adding mockito to use it in unit test

--- test/features/home/home_view_model_test.dart
+++ test/features/home/home_view_model_test.dart
@@ -1,18 +1,24 @@
 import 'package:book_store_app/core/data/api_response.dart';
 import 'package:book_store_app/data/model/book.dart';
+import 'package:book_store_app/domain/repository/home_repository.dart';
 import 'package:book_store_app/domain/use_cases/home/home_use_case.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-import 'fake_home_repository.dart';
+import 'home_view_model_test.mocks.dart';
 
+@GenerateMocks([HomeRepository])
 void main() {
   test('should return success when fake repo returns success', () async {
-    FakeHomeRepository fakeHomeRepository = FakeHomeRepository();
-    HomeUseCase homeUseCase = HomeUseCase(fakeHomeRepository);
+    MockHomeRepository mockHomeRepository = MockHomeRepository();
+    HomeUseCase homeUseCase = HomeUseCase(mockHomeRepository);
     final books = [Book(title: 'test', releaseDate: 'Jun 26,1997')];
-    fakeHomeRepository.setResponse(ApiResponse.success(books));
-    final result = await homeUseCase.execute();
+    when(
+      mockHomeRepository.getListOfBooks(),
+    ).thenAnswer((_) async => ApiResponse.success(books));
 
+    final result = await homeUseCase.execute();
     expect(result.isSuccess, true);
     expect(result.data?.length, 1);
   });
